62b799982872911249e00026aa76dfb7ff379eff
Dan Hildebrandt <hildy@webgroupmedia.com>
2008-08-13 21:41:10 +0000

    * hash checks to prevent running on versions prior to 2.7 or 3.6

--- Cerb2Exporter/src/com/cerberusweb/cerb2/Driver.java
+++ Cerb2Exporter/src/com/cerberusweb/cerb2/Driver.java
@@ -1,10 +1,32 @@
 package com.cerberusweb.cerb2;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.cerberusweb.cerb2.Database;
 import com.cerberusweb.cerb2.entities.Ticket;
 import com.cerberusweb.cerb2.entities.Worker;
 
 public class Driver {
 	public Driver() {
+		// check db version; 2.7 required
+		try {
+			Connection conn = Database.getInstance();
+			Statement s = conn.createStatement();
+			s.execute("SELECT * FROM db_script_hash WHERE script_md5='fbdb155c25f4ba500442f8cfaf6bc9bc';"); // 2.7 clean hash
+			ResultSet rs = s.getResultSet();
+			
+			if (!rs.next()) {
+				System.err.println("3.6 db script hash not found!  Exiting...");
+				System.exit(1);
+			}
+			
+		} catch (SQLException sqlE) {
+			sqlE.printStackTrace();
+		}
+		
 		new Worker().export();
 //		new Address().export();
 		new Ticket().export();
--- Cerb3Exporter/src/com/cerb3/exporter/Driver.java
+++ Cerb3Exporter/src/com/cerb3/exporter/Driver.java
@@ -1,10 +1,31 @@
 package com.cerb3.exporter;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.cerb3.exporter.entities.Ticket;
 import com.cerb3.exporter.entities.Worker;
 
 public class Driver {
 	public Driver() {
+		// check db version; 3.6 required
+		try {
+			Connection conn = Database.getInstance();
+			Statement s = conn.createStatement();
+			s.execute("SELECT * FROM db_script_hash WHERE script_md5='a646f96e8f3bd6f7ced1737d389b1239';"); // 3.6 clean hash
+			ResultSet rs = s.getResultSet();
+			
+			if (!rs.next()) {
+				System.err.println("3.6 db script hash not found!  Exiting...");
+				System.exit(1);
+			}
+			
+		} catch (SQLException sqlE) {
+			sqlE.printStackTrace();
+		}
+		
 		new Worker().export();
 //		new Address().export();
 		new Ticket().export();
