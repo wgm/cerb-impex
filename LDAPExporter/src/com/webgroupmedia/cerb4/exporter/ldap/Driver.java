226a4828f3784d08502b2f166fe85ef9761bc589
Mike Fogg <mike@webgroupmedia.com>
2009-02-27 07:26:33 +0000

    * LDAP Exporter now also lets you export people as Workers in addition to contacts (it enforces one or the other). * Zendesk contact password defaults to an empty string instead of changeme * Zendesk contact passwords are no longer encrypted if they are an empty string * Fixed issue in Zendesk exporter where worker xml could have been generated in one file instead of a file for each worker * Made appropriate changes to ldap config and zendesk config (also removed default password setting for contacts, since it wouldn't make sense and if a blank password is imported it just requires the contact to sign up through Cerb4) * Built new jar files for zendesk and ldap exporters

--- LDAPExporter/src/com/webgroupmedia/cerb4/exporter/ldap/Driver.java
+++ LDAPExporter/src/com/webgroupmedia/cerb4/exporter/ldap/Driver.java
@@ -3,15 +3,21 @@ package com.webgroupmedia.cerb4.exporter.ldap;
 
 import com.cerb4.impex.Configuration;
 import com.webgroupmedia.cerb4.exporter.ldap.entities.Contact;
+import com.webgroupmedia.cerb4.exporter.ldap.entities.Worker;
 
 
 public class Driver {
 
 	public Driver() {
 		Boolean bExportContacts = new Boolean(Configuration.get("exportContacts", "false")); 
+		Boolean bExportWorkers = new Boolean(Configuration.get("exportWorkers", "false"));
 		
-		if(bExportContacts)
+		if(bExportWorkers)
+			new Worker().export();
+
+		if(bExportContacts && !bExportWorkers)
 			new Contact().export();
+		
 	}
 	
 }
